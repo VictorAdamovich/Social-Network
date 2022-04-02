d4f960a55969ab8467562f004724565871b319c9
Victor Adamovich <ndptrv@yandex.by>
2022-04-02 14:00:24 +0300

    Add project

---
+++ src/components/Profile.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div className="div"><img
+                src="https://media.cntraveler.com/photos/5eb18e42fc043ed5d9779733/16:9/w_2580,c_limit/BlackForest-Germany-GettyImages-147180370.jpg"
+                alt=""/>
+            </div>
+            <div>ava+disc</div>
+            <div>mypost</div>
+            <div>New post</div>
+            <div>Post1</div>
+            <div>Post2</div>
+        </div>
+    );
+};
+
+export default Profile;
