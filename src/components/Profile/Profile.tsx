f2831423973aa212b4152e0b7a6c0f9fadc0dc62
Victor Adamovich <ndptrv@yandex.by>
2022-04-09 20:11:39 +0300

    add 18 lesson

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -4,8 +4,8 @@ import MyPosts from "./MyPosts/Posts/MyPosts";
 
 const Profile = () => {
     return (
-        <div className={c.content}>
-            <div className="{classes.div}"><img
+        <div>
+            <div className="{c.content}"><img
                 src="https://media.cntraveler.com/photos/5eb18e42fc043ed5d9779733/16:9/w_2580,c_limit/BlackForest-Germany-GettyImages-147180370.jpg"
                 alt=""/>
             </div>
